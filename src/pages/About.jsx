7147750edcf792582f4e709a487b14924092b8d4
Muhamad Iqbal fauzi <miqbalf2021@gmail.com>
2024-05-09 15:11:36 +0700

    edit dikit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,8 @@ const About = () => {
         <h1 className='text-amber-400 font-bold text-2xl ml-9 mt-4'>About</h1>
         <p className='text-white ml-9 mt-4'>
         Aplikasi ini dibuat untuk memenuhi tugas UTS mata kuliah praktikum sistem multimedia.
-    Aplikasi ini dirancang untuk membantu pengguna dalam mengolah dan memodifikasi media gambar dan audio.
+        Aplikasi ini dirancang untuk membantu pengguna dalam mengolah dan memodifikasi media gambar dan audio.
+        Aplikasi ini dibuat Oleh Muhamad Iqbal fauzi 1217050085, UIN Sunan Gunung Djati Bandung
         </p>
         </div>
         
